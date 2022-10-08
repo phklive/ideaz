171f5837c7a7bbe3721654d256ef7520e6bb4993
phk.dev <kajfaszph@gmail.com>
2022-10-08 17:44:07 +0200

    yes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="grid grid-cols-3 px-4 py-4 text-xl mb-4">
-      <div>
-        <Link href="/">
-          <Image src="/logo.png" alt="logo" height={100} width={200} />
-        </Link>
-      </div>
-      <div className="flex flex-row items-center justify-center gap-20">
+    <nav className="text-xl my-4">
+      <div className="flex flex-row items-center justify-center gap-20 self-center">
         <Link href="/">Home</Link>
         <Link href="/ideas">Ideas</Link>
         <Link href="/learn">Learn</Link>
