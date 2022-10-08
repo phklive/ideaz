7379d94d0b334e0b715afc4c4cb0d02baf343f4d
jimmylies <tybernox@gmail.com>
2022-10-08 17:42:27 +0200

    feat: landingheader

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -3,18 +3,19 @@ import React from "react";
 
 const LandingHeader = () => {
   return (
-    <div className="">
-      <h1 className="text-center text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-7xl font-bold">
-        Matching <em className="font-bold">best projects's</em> with{" "}
-        <em className="font-bold">best builders</em>
-      </h1>
-      <Image
-        src="/flying.png"
-        alt="flying illustration"
-        className="flying"
-        height={500}
-        width={500}
-      />
+    <div className="flex flex-col">
+      <div className="flex justify-center align-center flex-col">
+        <h1 className="text-center text-4xl font-normal">
+          Matching best projects's with best builders
+        </h1>
+      </div>
+        <Image
+          src="/flying.png"
+          alt="flying illustration"
+          className="flying"
+          width={500}
+          height={500}
+        />
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,12 @@ const Home: NextPage = () => {
       <Head>
         <title>Ideaz</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Questrial&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <LandingHeader />
       <Users />
