693d77d428919fd5b628a68ce45843f29d453423
phk.dev <kajfaszph@gmail.com>
2022-10-08 19:50:27 +0200

    pull

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -6,7 +6,7 @@ const LandingHeader = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center">
       <div className="flex flex-col justify-center items-center">
         <Image src={"/logo.png"} height={228} width={400} />
-        <h1 className="text-center text-5xl font-normal mt-4">
+        <h1 className="text-center text-5xl font-normal mt-8">
           Matching best <span className="font-bold">Projects 🚀</span> <br />{" "}
           with best <span className="font-bold">Builders 👷🏽‍♂️</span>
         </h1>
