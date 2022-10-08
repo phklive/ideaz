41f9fd426592248d415ffd291b0063198f5b400c
phk.dev <kajfaszph@gmail.com>
2022-10-08 17:58:31 +0200

    landing

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -3,19 +3,21 @@ import React from "react";
 
 const LandingHeader = () => {
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-center align-center flex-col">
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col justify-center items-center">
+        <Image src={"/logo.png"} height={171} width={300} />
         <h1 className="text-center text-4xl font-normal">
-          Matching best projects's with best builders
+          Matching best <span className="font-bold">Projects 🚀</span> <br />{" "}
+          with best <span className="font-bold">Builders 👷🏽‍♂️</span>
         </h1>
       </div>
-        <Image
-          src="/flying.png"
-          alt="flying illustration"
-          className="flying"
-          width={500}
-          height={500}
-        />
+      <Image
+        src="/flying.png"
+        alt="flying illustration"
+        className="flying"
+        width={500}
+        height={500}
+      />
     </div>
   );
 };
