e7c6776f4ee5047ee30282cdca3529b281ace3e3
phk.dev <kajfaszph@gmail.com>
2022-10-08 22:08:18 +0200

    push

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="w-11/12 mx-auto">
+    <div className="w-11/12 mx-auto mb-10">
       <Navbar />
       <Component {...pageProps} />
     </div>
