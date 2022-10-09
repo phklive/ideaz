cbf808c37eb8209be1174a22a92d09ef05117108
jimmylies <tybernox@gmail.com>
2022-10-09 06:45:15 +0200

    style: loginButton

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,45 +3,46 @@ import React, { useContext, useEffect, useState } from "react";
 import TezosContext from "../utils/context";
 
 export default function LoginButton() {
-    const [account, setAccount] = useState<AccountInfo>();
-    const context = useContext(TezosContext);
+  const [account, setAccount] = useState<AccountInfo>();
+  const context = useContext(TezosContext);
 
-    const connect = async () => {
-        context.client.getActiveAccount().then(async (acc) => {
-            if (acc) setAccount(acc);
-            else
-                await context
-                    .requestPermissions({
-                        // network: {
-                        //     type: NetworkType.GRANADANET,
-                        //     rpcUrl: "https://granadanet.smartpy.io",
-                        // },
-                    })
-                    .then(() => connect());
-        });
-    };
+  const connect = async () => {
+    context.client.getActiveAccount().then(async (acc) => {
+      if (acc) setAccount(acc);
+      else
+        await context
+          .requestPermissions({
+            // network: {
+            //     type: NetworkType.GRANADANET,
+            //     rpcUrl: "https://granadanet.smartpy.io",
+            // },
+          })
+          .then(() => connect());
+    });
+  };
 
-    // useEffect(() => {
-    //     connect();
-    // }, []);
+  // useEffect(() => {
+  //     connect();
+  // }, []);
 
-    const handleLogin = () => {
-        if (account) {
-            context.client.clearActiveAccount();
-            setAccount(undefined);
-        } else {
-            connect();
-        }
-    };
+  const handleLogin = () => {
+    if (account) {
+      context.client.clearActiveAccount();
+      setAccount(undefined);
+    } else {
+      connect();
+    }
+  };
 
-    return (
-        <>
-            <span>
-                {account && `Connected as ${account.address.slice(0, 6)}...`}
-            </span>
-            <button onClick={handleLogin}>
-                {account ? "Disconnect" : "Connect"}
-            </button>
-        </>
-    );
+  return (
+    <>
+      <span onClick={handleLogin}>
+        {account &&
+          `Connected: ${account.address.slice(0, 3)}...${account.address.slice(
+            account.address.length - 3
+          )}`}
+      </span>
+      <button onClick={handleLogin}>{account ? "" : "Connect"}</button>
+    </>
+  );
 }
