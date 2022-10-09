387a4e3ea758a2d006039968e9e1d1fc855ec21a
jimmylies <tybernox@gmail.com>
2022-10-09 08:00:46 +0200

    style: learn style

--- src/pages/learn/[id].tsx
+++ src/pages/learn/[id].tsx
@@ -6,69 +6,121 @@ import Image from "next/image";
 import Modal from "../../components/Modal";
 
 const Course = () => {
-    const [course, setCourse] = useState<Course>();
-    const [activeQuestion, setActiveQuestion] = useState(0);
-    const [valid, setValid] = useState<boolean>();
-    const [showModal, setShowModal] = useState(false);
-    const router = useRouter();
+  const [course, setCourse] = useState<Course>();
+  const [activeQuestion, setActiveQuestion] = useState(-1);
+  const [valid, setValid] = useState<boolean>();
+  const [showModal, setShowModal] = useState(false);
+  const [check, setCheck] = useState([false]);
+  const router = useRouter();
 
-    const checkAnswers = () => {
-        setShowModal(true);
-        setValid(Math.random() > 0.5);
-    };
+  const checkAnswers = () => {
+    setShowModal(true);
+    setValid(Math.random() > 0.5);
+  };
 
-    useEffect(() => {
-        if (!router.isReady) return;
+  useEffect(() => {
+    if (!router.isReady) return;
 
-        router.query.id &&
-            !Array.isArray(router.query.id) &&
-            setCourse(courses.at(parseInt(router.query.id)));
-    }, [router.isReady]);
+    router.query.id &&
+      !Array.isArray(router.query.id) &&
+      setCourse(courses.at(parseInt(router.query.id)));
+  }, [router.isReady]);
 
-    if (!course) return <div>Loading...</div>;
+  if (!course) return <div>Loading...</div>;
 
-    return (
-        <div>
-            <div className="flex flex-col justify-center items-center h-80 mt-32 mb-16 gap-2">
-                <h1 className="text-5xl">{course.name}</h1>
-                <Image
-                    className="flying"
-                    src="/book.png"
-                    width={226}
-                    height={226}
-                />
-                <h2 className="text-xl">{course.description}</h2>
-                <a href={course.docURL} className="flex flex-row">
-                    Documentation <AiOutlineLink />
-                </a>
+  return (
+    <div>
+      {activeQuestion === -1 && (
+        <>
+          <div className="flex flex-col justify-center items-center h-80 mt-32 mb-16 gap-2">
+            <h1 className="text-5xl">{course.name}</h1>
+            <Image
+              className="flying"
+              src="/book.png"
+              width={226}
+              height={226}
+            />
+            <h2 className="text-xl">{course.description}</h2>
+            <a href={course.docURL} className="flex flex-row">
+              Documentation <AiOutlineLink />
+            </a>
+          </div>
+        </>
+      )}
+
+      <div className="flex justify-center items-center">
+        {activeQuestion === -1 ? (
+          <div
+            className="w-36 text-center rounded bg-gray-50 px-3 py-1 cursor-pointer"
+            onClick={() => setActiveQuestion(0)}
+          >
+            Start questions
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center h-screen w-screen gap-16">
+            <div className="flex flex-col justify-center items-center">
+              <h3 className="mt-24 text-3xl text-center">
+                {course.questions[activeQuestion].question}
+              </h3>
+              <a href={course.docURL} className="flex flex-row mt-4">
+                Documentation <AiOutlineLink />
+              </a>
             </div>
-
-            <div>
-                <h3>{course.questions[activeQuestion].question}</h3>
+            <div className="flex flex-col items-center justify-center w-full">
+              <div className="grid grid-cols-2 gap-2 w-full">
                 {course.questions[activeQuestion].answers
-                    .concat(course.questions[activeQuestion].wrongAnswers)
-                    .map((answer, j) => (
-                        <div key={j}>
-                            <input className="mr-2" type="checkbox" />
-                            <label htmlFor={j.toString()}>{answer}</label>
-                        </div>
-                    ))}
-                <button
-                    onClick={() => {
-                        if (activeQuestion == course.questions.length - 1)
-                            checkAnswers();
-                        else setActiveQuestion((prev) => prev + 1);
-                    }}
-                    className="rounded bg-gray-50 px-3 py-1 mx-auto my-0"
-                >
-                    Next question
-                </button>
-            </div>
-
-            {/* <div className="grid grid-cols-2 gap-12 justify-start align-center mb-8">
-                {course.questions.map((question, i) => (
+                  .concat(course.questions[activeQuestion].wrongAnswers)
+                  .map((answer, j) => (
                     <div
-                        key={i}
+                      key={j}
+                      className="bg-gray-200 p-3 rounded cursor-pointer"
+                    >
+                      <input
+                        className="mr-4 ml-3"
+                        type="checkbox"
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                          e.currentTarget.parentElement?.classList.toggle(
+                            "bg-blue-100"
+                          );
+                        }}
+                      />
+                      <label htmlFor={j.toString()}>{answer}</label>
+                    </div>
+                  ))}
+              </div>
+              <div className="flex flex-row gap-2">
+                {activeQuestion !== 0 && (
+                  <button
+                    onClick={() => {
+                      setActiveQuestion((prev) => prev - 1);
+                    }}
+                    className="rounded bg-gray-50 px-3 py-1 mx-auto my-8"
+                  >
+                    Last question
+                  </button>
+                )}
+                <button
+                  onClick={() => {
+                    if (activeQuestion == course.questions.length - 1)
+                      checkAnswers();
+                    else setActiveQuestion((prev) => prev + 1);
+                  }}
+                  className="rounded bg-gray-50 px-3 py-1 mx-auto my-8"
+                >
+                  {activeQuestion == course.questions.length - 1
+                    ? "Validate answers"
+                    : "Next question"}
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+
+      {/* <div className="grid grid-cols-2 gap-12 justify-start align-center mb-8">
+      {course.questions.map((question, i) => (
+          <div
+          key={i}
                         className="flex flex-col course p-4 rounded shadow-xl"
                     >
                         <h3 className="mb-2">{question.question}</h3>
@@ -91,7 +143,7 @@ const Course = () => {
                 ))}
             </div> */}
 
-            {/* <div className="flex flex-col items-center justify-center gap-2">
+      {/* <div className="flex flex-col items-center justify-center gap-2">
                 <button
                     onClick={() => checkAnswers()}
                     className="rounded bg-gray-50 px-3 py-1 mx-auto my-0"
@@ -100,21 +152,43 @@ const Course = () => {
                 </button>
             </div> */}
 
-            {showModal && (
-                <Modal setShowModal={setShowModal}>
-                    {valid ? (
-                        <div className="rounded px-3 py-1 mx-auto my-0 text-green-600">
-                            Correct
-                        </div>
-                    ) : (
-                        <div className="rounded px-3 py-1 mx-auto my-0 text-red-600">
-                            Incorrect
-                        </div>
-                    )}
-                </Modal>
-            )}
-        </div>
-    );
+      {showModal && (
+        <Modal setShowModal={setShowModal}>
+          {valid ? (
+            <div className="flex flex-col items-center justify-center">
+              <div className="rounded px-3 py-1 mx-auto my-0 text-green-600">
+                Correct
+              </div>
+              <span className="text-center">
+                Congratulation you pass the certification for the {course.name}{" "}
+                course!
+              </span>
+              <span>
+                Get the certification proof by taking the {course.name} NFT
+              </span>
+              <Image src="/blockchain_basics.png" width={226} height={226} />
+              <button
+                onClick={() => console.log("transaction pending...")}
+                className="rounded bg-gray-50 px-3 py-1 mx-auto mb-4 bg-blue-300"
+              >
+                Take it
+              </button>
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center">
+              <div className="rounded px-3 py-1 mx-auto my-0 text-red-600">
+                Incorrect
+              </div>
+              <span className="text-center">
+                You need to pass the questions perfectly to get the
+                certification proof.
+              </span>
+            </div>
+          )}
+        </Modal>
+      )}
+    </div>
+  );
 };
 
 export default Course;
